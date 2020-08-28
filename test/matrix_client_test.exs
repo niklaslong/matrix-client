55fef02eee08fd00b912608716b94218f969c37d
Harley Swick <fancycade@protonmail.com>
2020-08-27 21:47:30 -0500

    login and logout tests

--- test/matrix_client_test.exs
+++ test/matrix_client_test.exs
@@ -7,4 +7,16 @@ defmodule MatrixClientTest do
     {:ok, body} = MatrixClient.register_user(pid, Rando.string(), Rando.string())
     assert body.status == 200
   end
+
+  test "login and logout" do
+    {:ok, pid} = MatrixClient.new_session("http://localhost:8008")
+    username = Rando.string()
+    password = Rando.string()
+    {:ok, _} = MatrixClient.register_user(pid, username, password)
+
+    :ok = MatrixClient.login_user(pid, username, password)
+
+    result = MatrixClient.logout(pid)
+    assert result.status == 200
+  end
 end
