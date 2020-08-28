0981a258b32d9c77eb42a7e4d6d95db9a38f8c94
Harley Swick <fancycade@protonmail.com>
2020-08-27 21:41:47 -0500

    Generate random username and password for tests

--- test/matrix_client_test.exs
+++ test/matrix_client_test.exs
@@ -4,7 +4,7 @@ defmodule MatrixClientTest do
 
   test "register user" do
     {:ok, pid} = MatrixClient.new_session("http://localhost:8008")
-    {:ok, body} = MatrixClient.register_user(pid, "foo", "bar")
+    {:ok, body} = MatrixClient.register_user(pid, Rando.string(), Rando.string())
     assert body.status == 200
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,27 @@
 ExUnit.start()
+
+# Mostly taken from here https://elixirforum.com/t/generating-5-000-random-strings-improving-performance-by-an-order-of-magnitude-but-can-it-be-better/23206/5
+
+defmodule Rando do
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+
+  @chars List.to_tuple(chars)
+
+  def string() do
+    generate_rand_string()
+  end
+  
+  defp generate_rand_string() do
+    0
+    |> generate_rand_string()
+    |> IO.iodata_to_binary()  
+  end
+
+  defp generate_rand_string(n) when n == 10 do
+    []
+  end
+
+  defp generate_rand_string(n) do 
+    [elem(@chars, :rand.uniform(35) - 1), generate_rand_string(n+1)]
+  end  
+end
